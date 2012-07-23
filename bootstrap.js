03f0901f169fff628947da467f2b55ec38492706
Thomas Bassetto <tbassetto@gmail.com>
2012-07-23 15:29:19 +0200

    Update to match latest addon API

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,11 @@
 define([], function () {
-  return function(runtime, params, callback) {
-    runtime.readFile('./toInject.js', function(err, code) {
+  return function (runtime, params, callback) {
+    runtime.readFile('toInject.js', function (err, code) {
       // Unfortunately, our injected file do not have access to `params`
       code = code.replace('_APP_KEY_', params.options.pusherAppKey);
       code = code.replace('_SECRET_', params.options.secret);
-      callback(null, params.content + code);
+      params.content += code;
+      callback();
     });
   };
 });
