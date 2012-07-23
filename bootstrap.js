115378b691702f0ed6c4e5bba6833b58f16c0760
Thomas Bassetto <tbassetto@gmail.com>
2012-07-23 15:50:50 +0200

    Fix regexp

--- bootstrap.js
+++ bootstrap.js
@@ -2,9 +2,8 @@ define([], function () {
   return function (runtime, params, callback) {
     runtime.readFile('toInject.js', function (err, code) {
       // Unfortunately, our injected file do not have access to `params`
-      code = code.replace('_APP_KEY_', params.options.pusherAppKey);
-      var secret = new RegExp(params.options.secret, 'gi');
-      code = code.replace('_SECRET_', secret);
+      code = code.replace('/_APP_KEY_/gi', params.options.pusherAppKey);
+      code = code.replace('/_SECRET_/gi', params.options.secret);
       params.content += code;
       callback();
     });
