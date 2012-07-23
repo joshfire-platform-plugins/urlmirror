1c08e5fdf80b4f2eec83a7727f22aff914a56469
Thomas Bassetto <tbassetto@gmail.com>
2012-07-23 15:46:26 +0200

    Replace secret password globally

--- bootstrap.js
+++ bootstrap.js
@@ -3,7 +3,8 @@ define([], function () {
     runtime.readFile('toInject.js', function (err, code) {
       // Unfortunately, our injected file do not have access to `params`
       code = code.replace('_APP_KEY_', params.options.pusherAppKey);
-      code = code.replace('_SECRET_', params.options.secret);
+      var secret = new RegExp(params.options.secret, 'gi');
+      code = code.replace('_SECRET_', secret);
       params.content += code;
       callback();
     });
