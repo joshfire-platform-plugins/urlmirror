3d0dfeab99345bc05c6c7232e5eb972a264e281a
Thomas Bassetto <tbassetto@gmail.com>
2012-07-23 15:56:56 +0200

    Remobe quote around regexp

--- bootstrap.js
+++ bootstrap.js
@@ -2,8 +2,8 @@ define([], function () {
   return function (runtime, params, callback) {
     runtime.readFile('toInject.js', function (err, code) {
       // Unfortunately, our injected file do not have access to `params`
-      code = code.replace('/_APP_KEY_/gi', params.options.pusherAppKey);
-      code = code.replace('/_SECRET_/gi', params.options.secret);
+      code = code.replace(/_APP_KEY_/gi, params.options.pusherAppKey);
+      code = code.replace(/_SECRET_/gi, params.options.secret);
       params.content += code;
       callback();
     });
